9ea764d56b14454f44a8e35c77234daa12599808
Christian Thomas <christian@chr-thomas.de>
2012-12-21 20:40:55 +0100

    new release of project - following problems exists: - If a new article is created, it will be created again, if the browser will be refreshed - currently not be able to edit an existing article --> see MyStatefulSnippets class -> edit method

--- src/main/scala/code/model/Articles.scala
+++ src/main/scala/code/model/Articles.scala
@@ -5,13 +5,15 @@
 
 package code.model
 
+import collection.mutable.ListBuffer
+
 case class Article(id: Long, title: String, content: String)
 
 object Articles {
 
-  var articles = List(Article(0, "Peter Parker", "Is spiderman."),
-											Article(1, "Bruce Wayne", "Is the batman! The only one dark knight."),
-											Article(2, "Clark Kent", "Is superman. The strongest man of the world."))
+  var articles = ListBuffer(Article(0, "Peter Parker", "Is spiderman."),
+										 	 			Article(1, "Bruce Wayne", "Is the batman! The only one dark knight."),
+										  			Article(2, "Clark Kent", "Is superman. The strongest man of the world."))
   
   /**
    * Note that list-operation :+ has a complexity of O(n). If you need this operation frequently, 
@@ -19,10 +21,18 @@ object Articles {
    */
   def add(article: Article) {
     // TODO actually this method does not work ! Fix it...
-		println("Incoming article=" + article)
-    articles ::: List(article)
-    println("following articles exist's=" + articles)
+		println("Incoming article to add=" + article)
+		articles = articles :+ article
+		println("following articles exist's=" + articles)
   }
+
+	def remove(article: Article) {
+		println("Incoming article to add=" + article)
+		articles = articles - article
+		println("following articles exist's=" + articles)
+	}
+
+	def getArticlesList = articles.toList
 
   def generateId() : Long = articles.length
   
--- src/main/scala/code/snippet/MyStatefulSnippets.scala
+++ src/main/scala/code/snippet/MyStatefulSnippets.scala
@@ -19,8 +19,8 @@ class MyArticleStateful extends StatefulSnippet {
 		var title = ""
 		var content = ""
 
-    "name=title" #> SHtml.text(title, title = _) &
-    "name=content" #> SHtml.textarea(content, content = _) &
+    "@title" #> SHtml.text(title, title = _) &
+    "@content" #> SHtml.textarea(content, content = _) &
     "type=submit" #> SHtml.onSubmitUnit(() => save(title, content))
 	}
 
@@ -38,7 +38,7 @@ class MyTableStateful extends StatefulSnippet {
 	// to the method.
 	def dispatch = {case _ => render}
 
-	def render = "* *" #> Articles.articles.map{renderArticle(_)}
+	def render = "* *" #> Articles.getArticlesList.map{renderArticle(_)}
 
 	/**
 	 * Function should render a table row for given article. Also it will bind the article and the appropriate input
@@ -50,37 +50,22 @@ class MyTableStateful extends StatefulSnippet {
 		var inputContent = article.content
 
 		"@id [value]" #> article.id &
-			"@title [value]" #>  SHtml.text(inputTitle, inputTitle = _) &
-			"@content [value]" #> SHtml.text(inputContent, inputContent = _) &
+			"@title" #>  SHtml.text(inputTitle, inputTitle = _) &
+			"@content" #> SHtml.text(inputContent, inputContent = _) &
 			"@edit-button" #> SHtml.onSubmitUnit(() => edit(article, inputTitle, inputContent)) &
 			"@remove-button" #> SHtml.onSubmitUnit(() => remove(article))
 	}
 
 	private def edit(article: Article, inputTitle: String, inputContent: String) {
 		println("--> edit article=" + article + ", inputTitle=" + inputTitle + ", inputContent=" + inputContent)
+// FIXME error: reassignment to val
+//		article.title = inputTitle
+//		article.content = inputContent
 	}
 
 	private def remove(article: Article) {
 		println("--> remove article=" + article)
+		Articles.remove(article)
 	}
-
-//	def render =
-//		"* *" #> Articles.articles.map{article =>
-//			"@id [value]" #> article.id &
-//				"@title [value]" #>  article.title &
-//				"@content [value]" #> article.content &
-//				"@edit-button [onclick]" #> SHtml.ajaxInvoke(() => edit(article)) &
-//				"@remove-button [onclick]" #> SHtml.ajaxInvoke(() => remove(article))
-//		}
-//
-//	private def edit(article: Article) : JsCmd = {
-//		println("--> edit article=" + article)
-//		Noop
-//	}
-//
-//	private def remove(article: Article) : JsCmd = {
-//		println("--> remove article=" + article)
-//		Noop
-//	}
 
 }
