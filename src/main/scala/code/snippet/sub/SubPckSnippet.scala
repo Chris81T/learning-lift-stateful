d693a4c327b6af0da3f2116ba483900a00cd73b1
Christian Thomas <christian@chr-thomas.de>
2012-12-29 20:11:28 +0100

    add tiny sub-package snippet example. Acutally this does not work?! The reason for the error is class not found.

---
+++ src/main/scala/code/snippet/sub/SubPckSnippet.scala
@@ -0,0 +1,15 @@
+package code.snippet.sub
+
+import net.liftweb._
+import util.Helpers._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: christian
+ * Date: 12/29/12
+ * Time: 7:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class SubPckSnippet {
+	  def render = "*" #> "Hello sub package snippet"
+}
